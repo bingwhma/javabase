468c9edb62d76fd078c6eb7e81a5ec3d10d8acce
linyi12 <bingm@hpe.com>
2017-07-07 12:50:59 +0800

    init

---
+++ lambda/src/main/java/OptionalsTest.java
@@ -0,0 +1,16 @@
+import java.util.Optional;
+
+/**
+ * Created by mabi on 2017/07/07.
+ */
+public class OptionalsTest {
+
+    public static void main(String[] args) {
+        // Optional仅仅是一个容易：存放T类型的值或者null。它提供了一些有用的接口来避免显式的null检查，可以参考Java 8官方文档了解更多细节。
+
+        Optional< String > fullName = Optional.ofNullable( null );
+        System.out.println( "Full Name is set? " + fullName.isPresent() );
+        System.out.println( "Full Name: " + fullName.orElseGet( () -> "[none]" ) );
+        System.out.println( fullName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+    }
+}
