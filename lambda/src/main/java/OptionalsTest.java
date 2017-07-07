9bc733c629d5f7b40cc3266e0a492e49e2c04abf
linyi12 <bingm@hpe.com>
2017-07-07 13:41:51 +0800

    add new function

--- lambda/src/main/java/OptionalsTest.java
+++ lambda/src/main/java/OptionalsTest.java
@@ -8,9 +8,34 @@ public class OptionalsTest {
     public static void main(String[] args) {
         // Optional仅仅是一个容易：存放T类型的值或者null。它提供了一些有用的接口来避免显式的null检查，可以参考Java 8官方文档了解更多细节。
 
-        Optional< String > fullName = Optional.ofNullable( null );
-        System.out.println( "Full Name is set? " + fullName.isPresent() );
-        System.out.println( "Full Name: " + fullName.orElseGet( () -> "[none]" ) );
-        System.out.println( fullName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+//        Optional< String > fullName = Optional.ofNullable( null );
+//        System.out.println( "Full Name is set? " + fullName.isPresent() );
+//        System.out.println( "Full Name: " + fullName.orElseGet( () -> "[none]" ) );
+//        System.out.println( fullName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+
+        Integer value1 = null;
+        Integer value2 = new Integer(10);
+
+        // Optional.ofNullable - 允许传递为 null 参数
+        Optional<Integer> a = Optional.ofNullable(value1);
+
+        // Optional.of - 如果传递的参数是 null，抛出异常 NullPointerException
+        Optional<Integer> b = Optional.of(value2);
+        System.out.println(sum(a,b));
+    }
+
+    public static Integer sum(Optional<Integer> a, Optional<Integer> b){
+
+        // Optional.isPresent - 判断值是否存在
+
+        System.out.println("第一个参数值存在: " + a.isPresent());
+        System.out.println("第二个参数值存在: " + b.isPresent());
+
+        // Optional.orElse - 如果值存在，返回它，否则返回默认值
+        Integer value1 = a.orElse(new Integer(0));
+
+        //Optional.get - 获取值，值需要存在
+        Integer value2 = b.get();
+        return value1 + value2;
     }
 }
